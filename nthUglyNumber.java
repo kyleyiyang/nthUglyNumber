d1628fd34a0ff66d3291c8ba958d69cce37f51de
kyleyiyang <kyleyiyang@gmail.com>
2021-05-23 22:26:45 -0400

    Update nthUglyNumber.java

--- nthUglyNumber.java
+++ nthUglyNumber.java
@@ -14,3 +14,18 @@ class Solution {
         return ugly[n-1];
     }
 }
+************************************
+
+class Solution {
+    public int nthUglyNumber(int n) {
+        TreeSet<Long> set=new TreeSet<>();
+        set.add(1L);
+        for (int i=0;i<n-1;i++) {
+            long temp=set.pollFirst();
+            set.add(temp*2);
+            set.add(temp*3);
+            set.add(temp*5);
+        }
+        return set.first().intValue();
+    }
+}
